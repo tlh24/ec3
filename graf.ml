844dc97256ebb6aa6121e4fa5927705278333f6f
Tim Hanson <sideskate@gmail.com>
2026-03-10 21:25:20 -0700

    small changes; really need aconceptual redesign

--- graf.ml
+++ graf.ml
@@ -578,6 +578,8 @@ let load gs fname =
 						| `Uniq -> gs.num_uniq <- gs.num_uniq + 1; (gs.num_uniq - 1)
 						| `Equiv -> gs.num_equiv <- gs.num_equiv + 1; (-1)
 						| _ -> (-1) in
+					(* need to eliminate leading `Seq objects with only one element *)
+
 					let ed,_ = pro_to_edata pro 0 in
 					if progt <> `Np then (
 						let d = {progt; ed; imgi ; outgoing; equivalents; equivroot; good} in
